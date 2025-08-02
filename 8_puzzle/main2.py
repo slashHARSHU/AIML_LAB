f6f0db07a804f9bd119ab6f30b360ee83daedb9e
harshad <harshadg300@gmail.com>
2025-08-02 15:27:05 +0530

    Files shifted to proper folder

--- 8_puzzle/main2.py
+++ 8_puzzle/main2.py
@@ -21,32 +21,6 @@ queue = hlp.np.concatenate([queue, start_state.reshape(1, 3, 3)], axis=0)
 
 # start the while loop and make the start state as current state
 current_state = start_state.copy()
-# while not hlp.np.array_equal(current_state, end_state):
-
-#     # get the new nodes for current state
-#     new_states = hlp.move_empty_tile(current_state)
-
-#     # remove any state if it is similar to the removed states
-#     count_list = []
-#     for i, state in enumerate(new_states):
-#         for removed_state in removed_state_queue:
-#             if hlp.np.array_equal(state, removed_state):
-#                 count_list.append(i)
-#                 break
-
-#     new_states = hlp.np.delete(new_states, count_list, axis=0)
-
-#     # store the removed states in
-#     removed_state_queue = hlp.np.concatenate(
-#         [removed_state_queue, current_state.reshape(1, 3, 3)], axis=0)
-
-#     # remove the first entry of queue
-#     queue = hlp.np.delete(queue, 0, axis=0)
-#     # add new states to queue
-#     queue = hlp.np.concatenate([queue, new_states], axis=0)
-#     # assign the next state as current state
-#     current_state = queue[0]
-#     print(current_state)
 
 while not hlp.heuristic(current_state, end_state) == 0:
     new_states = hlp.move_empty_tile(current_state)
